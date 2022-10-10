8c4f8c4535ba172d7882d9e489e56a608fc51cb4
AshishKerikere <ashishkerikere@gmail.com>
2022-10-10 15:18:40 +0530

    AnotherDocument

---
+++ PracticeDocument.sh
@@ -0,0 +1,5 @@
+                                                                   GIT Merge Practice
+This a Sample Document.
+This will used for GIT Merge Practice.
+The changes which will be made in this file will be reflected.
+The main file will be stored in the Master Branch.
